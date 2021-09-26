d74f30d88ed61460333b07fd7c80be124111b2bf
Ivan Nazarov <ivan.nazarov@lanars.com>
2021-09-26 14:52:06 +0300

    Create grouping by street

--- work_1/src/main/java/Main.java
+++ work_1/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,12 +9,14 @@ public class Main {
                 new MenuItem(
                     "Add",
                     () -> {
-                        String name = System.console().readLine("Name: ");
-                        String surname = System.console().readLine("Surname: ");
-                        int genderIndex = ConsoleUtil.readInt("Gender: \n1. Male\n2. Female\n-->", 1, 2);
+                        String name = ConsoleUtil.console().readLine("Name: ");
+                        String surname = ConsoleUtil.console().readLine("Surname: ");
+                        int genderIndex = ConsoleUtil.readInt(
+                                "Gender: \n1. Male\n2. Female\n-->",
+                                1, 2);
                         Gender gender = genderIndex == 1 ? Gender.MALE : Gender.FEMALE;
-                        String city = System.console().readLine("City: ");
-                        String street = System.console().readLine("Street: ");
+                        String city = ConsoleUtil.console().readLine("City: ");
+                        String street = ConsoleUtil.console().readLine("Street: ");
 
                         people.add(new Person(
                                 name,
@@ -30,7 +33,31 @@ public class Main {
                         for (Person person : people) {
                             System.out.println(person);
                         }
-                        System.console().readLine();
+                        ConsoleUtil.console().readLine();
+                    },
+                    true),
+                new MenuItem(
+                    "Group by street",
+                    () -> {
+                        HashMap<String, ArrayList<Person>> groups = new HashMap<>();
+                        for (Person person : people) {
+                            final String key = person.city() + " " + person.street();
+                            ArrayList<Person> group =  groups.get(key);
+                            if(group == null) {
+                                group = new ArrayList<>();
+                            }
+                            group.add(person);
+                            groups.put(key, group);
+                        }
+
+                        groups.forEach((key, group) -> {
+                            System.out.println(key);
+                            for (Person person : group) {
+                                System.out.println(person);
+                            }
+                            System.out.println();
+                        });
+                        ConsoleUtil.console().readLine();
                     },
                     true),
                 new MenuItem("Exit")
